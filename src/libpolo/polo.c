992b7b201a9d06485df3cf7cfa5042a50f7a5415
Alejandro Luebs <alejandroluebs@gmail.com>
2012-10-27 16:26:46 -0300

    Fixed libpolo issue

--- src/libpolo/polo.c
+++ src/libpolo/polo.c
@@ -527,8 +527,8 @@ void drawLine(float x1, float y1, float x2, float y2)
 	if (!poloState.isInitialized)
 		return;
 	
-	x2 += (x2 < x1) ? -1.0 : 1.0;
-	y2 += (y2 < y1) ? -1.0 : 1.0;
+	x2 += (x2 == x1) ? 0 : ((x2 < x1) ? -1.0 : 1.0);
+	y2 += (y2 == y1) ? 0 : ((y2 < y1) ? -1.0 : 1.0);
 	
 	/* Translate coordinates to pixel center */
 	glBegin(GL_LINES);
