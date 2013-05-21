9fc689a8fae038d23684b64f40dde7f385682842
Pablo Carranza Vélez <pablocarranza@gmail.com>
2013-05-21 15:36:34 -0300

    Calling close() on open files. Added MiniWahoo.

--- src/MiniWahoo.hpp
+++ src/MiniWahoo.hpp
@@ -1,11 +1,13 @@
-#include "wahoo.h"
+#ifndef MINIWAHOO_HPP
+#define MINIWAHOO_HPP
+
 #include <Configuration.hpp>
+#include <wahoo.h>
 #include <string>
 
 class MiniWahoo
 {
 	private:
-	
 		int channelAFile;
 		int channelBFile;
 		int channelADataFile;
@@ -13,17 +15,19 @@ class MiniWahoo
 		int triggerFile;
 		int sampleRateFile;
 		int resetFile;
-		
-		char *channelAData;
-		char *channelBData;
+		char* channelAData;
+		char* channelBData;
 		
 	public:
 		MiniWahoo(std::string dir);
 		~MiniWahoo(void);
-		char *getChannelDataBuffer(Configuration::Channels chan);
-		int fetchChannelSamples(Configuration::Channels chan);
-		bool setChannelConf(Configuration::Channels chan, char on,	unsigned char gain, char coupling, unsigned int offset);
-		bool setTriggerConf(Configuration::Channels chan, char slope, char aut, char roll, char noise, char hf, char holdOff, char level);
+		char* getChannelDataBuffer(Configuration::Channels chann);
+		int fetchChannelSamples(Configuration::Channels chann);
+		bool setChannelConf(Configuration::Channels chann, char on,	unsigned char gain, char coupling, unsigned int offset);
+		bool setTriggerConf(Configuration::Channels chann, char slope, char aut, char roll, char noise, char hf, char holdOff, char level);
 		bool setSampleRate(char sampleRate);
 		bool reset(void);
-}
+};
+
+#endif // MINIWAHOO_HPP
+
