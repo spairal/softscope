640803d168a66622eab7a90e992eda535fccdbe3
Pablo Carranza Vélez <pablocarranza@gmail.com>
2013-05-21 11:46:39 -0300

    Added wahoo.h

---
+++ src/wahoo.h
@@ -0,0 +1,29 @@
+/*
+*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct chan {
+	char id;
+	char on;
+	unsigned char gain;
+	char coupling;
+	unsigned int offset;
+};
+
+struct trig {
+	char channel;  // Channel for edge detection (0: A, 1: B)
+	char slope; 	// Trigger on rising (1) or falling (0) edge
+	char aut;		// Auto trigger on (1) or off (0)
+	char roll;		// Roll mode
+	char noise;		// Noise reject
+	char hf;			// High Frequency reject
+	char holdOff; 	// Hold-Off value
+	char level; 	// Trigger level value
+};
+
+#ifdef __cplusplus
+}
+#endif
